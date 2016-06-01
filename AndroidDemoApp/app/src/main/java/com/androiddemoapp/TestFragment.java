1770bc88c9352d5a0a8eb5f33e8da060c7520039
Karthik <Karthik@adeptpros.com>
2016-06-01 17:16:19 +0530

    Intial Commit Of Android Demo App

---
+++ AndroidDemoApp/app/src/main/java/com/androiddemoapp/TestFragment.java
@@ -0,0 +1,34 @@
+package com.androiddemoapp;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+/**
+ * Created by Karthik on 01/06/2016.
+ */
+public class TestFragment extends ListFragment implements AdapterView.OnItemClickListener {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+         View v=inflater.inflate(R.layout.listfragment,container,false);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.names, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
