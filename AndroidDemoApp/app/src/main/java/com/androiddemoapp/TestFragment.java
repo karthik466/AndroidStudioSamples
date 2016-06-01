dc93c090558ae146946695f13c89daf21b952b83
Karthik <karthik.cecc@gmail.com>
2016-06-01 17:30:44 +0530

    String.xml  and fragment changed

--- AndroidDemoApp/app/src/main/java/com/androiddemoapp/TestFragment.java
+++ AndroidDemoApp/app/src/main/java/com/androiddemoapp/TestFragment.java
@@ -7,28 +7,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
- * Created by Karthik on 01/06/2016.
+  @author Karthik
  */
 public class TestFragment extends ListFragment implements AdapterView.OnItemClickListener {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-         View v=inflater.inflate(R.layout.listfragment,container,false);
-        return v;
+
+        return inflater.inflate(R.layout.listfragment,container,false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.names, android.R.layout.simple_list_item_1);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.topic_names, android.R.layout.simple_list_item_1);
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), "Item: " + ((TextView)view).getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
